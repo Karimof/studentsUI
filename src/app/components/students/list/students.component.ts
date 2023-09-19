0f3035ec25562cdf13ee481788f84a0c0dc3b698
Karimof <islamkarimov8580@gmail.com>
2023-09-20 01:15:03 +0500

    make feature filtering

--- src/app/components/students/list/students.component.ts
+++ src/app/components/students/list/students.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {IStudents} from "../../../model/students-model";
 import {StudentServiceService} from "../../../services/students/student-service.service";
 import * as XLSX from 'xlsx';
+import {IStudies} from "../../../model/studies-model";
+import {StudiesService} from "../../../services/studies/studies.service";
 
 @Component({
   selector: 'app-students',
@@ -11,24 +13,37 @@ import * as XLSX from 'xlsx';
 export class StudentsComponent {
 
   studentList!: IStudents[];
+  filterStudentList!: IStudents[];
+  studies?: IStudies[];
+  studyId: any=0;
   fileName: string = "StudentList.xlsx"
 
   page: number = 1;
   count: number = 0;
   tableSize: number = 10;
+  defaultPressed: any = 0;
+  salom: any = "salom";
 
-  constructor(private studentService: StudentServiceService) {
+  constructor(private studentService: StudentServiceService, private studiesService: StudiesService) {
   }
 
   ngOnInit(): void {
     this.getStudentsList();
+    this.getStudies();
   }
 
   getStudentsList() {
     this.studentService.getStudents().subscribe(res => {
       if (res.body) {
         this.studentList = res.body!;
+        this.filterStudentList = this.studentList;
       }
+    })
+  }
+
+  getStudies() {
+    this.studiesService.getStudies().subscribe(value => {
+      this.studies = value.body!
     })
   }
 
@@ -51,5 +66,20 @@ export class StudentsComponent {
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
     XLSX.writeFile(wb, this.fileName);
+  }
+
+  filter() {
+    if (this.studyId == "" || this.studyId == 0) {
+      this.ngOnInit()
+    } else {
+      this.filterStudentList = this.studentList.filter(res => {
+        return res.studies?.id!.toString().match(this.studyId.toLowerCase())
+      })
+    }
+  }
+
+  clearFilter() {
+    this.studyId = 0;
+    this.ngOnInit()
   }
 }
